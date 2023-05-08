d07da67e0c314e49d25a15b94943720c68ee9231
Emanuel Júnior <117325229+EmanuelJnr@users.noreply.github.com>
2023-05-08 16:50:57 -0300

    ok
    ok

--- src/Lista10/EarthQuake.java
+++ src/Lista10/EarthQuake.java
@@ -1,7 +1,6 @@
 package Lista10;
 
 public class EarthQuake implements StrategyAtaque{
-	@Override
 	public void executarAtaque() {
 		System.out.println("Usando Earthquake");
 	}
--- src/Lista10/LeechSeed.java
+++ src/Lista10/LeechSeed.java
@@ -1,7 +1,6 @@
 package Lista10;
 
 public class LeechSeed implements StrategyAtaque{
-	@Override
 	public void executarAtaque() {
 		System.out.println("Usando Leech Seed");
 	}
